26b164b06b303a4c06e7c54f49913bd370d6195d
Kons M <katashi.rain@gmail.com>
2023-11-25 20:15:24 +0900

    Working request for shifts with some TODO

--- standby_api.py
+++ standby_api.py
@@ -1,17 +1,45 @@
 '''
 Fetch the shift data from Humanity API to create a report on standby shifts
-1. Request the data from API for a given period
-2. Convert the response into pandas df
-3. Modify the data to list
-    - Name, Number of shifts, Total hours, Weekday hours, Weekend hours
-    - Account for breaks
-4. Export the report to csv
+1. Create pandas df (Name, Hours, Shift, Start date, End date)
+2. Request the shift data from Humanity API
+    - specify period (start date to end date)
+    - specify positions (api accepts id)
+3. Parse the response data and insert items into pandas df
+    Each data point has employees
+        For each employee in this data point add and entry into df and include
+            - employees name (Name)
+            - employees paidtime (Hours)
+            - data schedule_name (Shift)
+            - data start_timestamp (Start date)
+            - data end_timestamp (End date)
+4. Group df by name to generate new output (Name, Number of shifts, Total hours, Weekday hours, Weekend hours)
+5. Export the output to csv
 '''
+import requests
+import json
+import datetime
 
-from fastapi import FastAPI
+if __name__ == '__main__':
+    ## Generate auth token
+    # Open the JSON file and load credentials   
+    with open('./auth/credentials.json') as json_file:
+        credentials = json.load(json_file)
+    oauth = 'https://www.humanity.com/oauth2/token.php'
+    response = requests.post(url=oauth, data=credentials)
+    auth_token = json.loads(response.text)['access_token']
 
-app = FastAPI()
-
-@app.get('/')
-async def root():
-    return {'message': 'Hello!'}
+    # Set request parameters
+    start_date = datetime.date(2024, 2, 4)
+    end_date = datetime.date(2024, 2, 5)
+    
+    positions = {'3110230': 'Cisco', '203110228': 'T1', '203110229': 'T2'}
+    schedule = '%2C%'.join(pos_id for pos_id in positions)
+    
+    api = f'https://www.humanity.com/api/v2/shifts?'
+    parameters = f'start_date={start_date}&end_date={end_date}&mode=schedule&schedule={schedule}&access_token={auth_token}'
+    url = api + parameters
+    
+    headers = {"accept": "application/json"}
+    
+    # Get shift data
+    shifts = requests.get(url, headers)
